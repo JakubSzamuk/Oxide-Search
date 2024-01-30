bf037db8c93fdc3d814a119833e809425be59915
jakubszamuk <lavaproofJakub1@gmail.com>
2024-01-30 21:08:08 +0000

    started parallelising

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::fs;
+use std::{fs, thread};
 use std::path::PathBuf;
+use std::sync::mpsc;
 
-fn mergeSort(arr: Vec<Token>) -> Vec<Token> {
+fn merge_sort(arr: Vec<Token>) -> Vec<Token> {
+    if arr.len() <= 1 {return arr};
     fn merge(mut arr1: Vec<Token>, mut arr2: Vec<Token>) -> Vec<Token> {
         let mut merged = Vec::new();
 
-        while (arr1.len() > 0 && arr2.len() > 0) {
-            if (arr1[0].val as u8 > arr2[0].val as u8) {
+        while arr1.len() > 0 && arr2.len() > 0 {
+            if arr1[0].val as u8 > arr2[0].val as u8 {
                 merged.push(arr2.remove(0));
             } else {
                 merged.push(arr1.remove(0));
@@ -16,49 +18,45 @@ fn mergeSort(arr: Vec<Token>) -> Vec<Token> {
         [merged, arr1, arr2].concat()
     }
 
-    let half = ((arr.len() as i16) / 2).floor();
-    let firstHalf = mergeSort(arr[0..half].to_vec());
-    let secondHalf = mergeSort(arr[half..arr.len()].to_vec());
 
-    return merge(firstHalf, secondHalf);
+    let handle_one = thread::spawn(move || {
+        merge_sort(arr[0..((arr.len() as f32) / 2.).floor() as usize].to_vec())
+    });
+    let handle_two = thread::spawn(move || {
+        merge_sort(arr[((arr.len() as f32) / 2.).floor() as usize..arr.len()].to_vec())
+    });
+
+    merge(handle_one.join().unwrap(), handle_two.join().unwrap())
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct Token {
     pub val: char,
     pub index: usize,
-    pub file: PathBuf
+    pub file: &'static str
 }
 
 fn index_files() {
     let files = fs::read_dir("./files").unwrap();
     let mut index_values = Vec::new();
     for filePath in files {
-        let filePathResult = filePath.unwrap().path();
-        let file = fs::read_to_string(&filePathResult).unwrap();
+        let file_path_result = filePath.unwrap().path();
+        let file = fs::read_to_string(&file_path_result).unwrap();
         for (index, character) in file.char_indices() {
             index_values.push(Token {
                 val: character,
                 index,
-                file: filePathResult.clone(),
+                file: file_path_result.to_str().unwrap(),
             });
         }
     }
     // let mut indexed_no_duplicates = Vec::new();
 
-    let indexed = mergeSort(index_values);
+    let indexed = merge_sort(index_values);
     println!("{:?}", indexed);
 
 
